47097f621ee1e3128a09aa0a556f00cdf09079f4
Quique1098 <144548709+Quique1098@users.noreply.github.com>
2025-07-13 19:01:04 -0300

    Score Event

--- Assets/Scripts/UI/WinoLose.cs
+++ Assets/Scripts/UI/WinoLose.cs
@@ -4,10 +4,11 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 using TMPro;
 using System;
-
+using Unity.Services.Analytics;
+using Unity.Services.Core;
+using Unity.Services.Authentication;
 public class WinoLose : MonoBehaviour
 {
-
     public static event Action OnWinEvent;
     public AdsManager adsManager;
 
@@ -23,22 +24,40 @@ public class WinoLose : MonoBehaviour
 
     public static void TriggerWin()
     {
-        OnWinEvent?.Invoke(); 
+        OnWinEvent?.Invoke();
     }
 
-    public void Win(){
+    public void Win()
+    {
+        EnviarEventoWin();
         SceneManager.LoadScene("WinMenu");
     }
 
-    public void Lose(){
-
+    public void Lose()
+    {
         SceneManager.LoadScene("LoseMenu");
     }
 
-    public void Main(){
+    public void Main()
+    {
         adsManager.ShowRewarded();
-
         SceneManager.LoadScene("MainMenu");
     }
 
+    private void EnviarEventoWin()
+    {
+        int finalScore = ScoreManager.Instance != null ? ScoreManager.Instance.score : 0;
+        string playerId = AuthenticationService.Instance.PlayerId;
+
+        var evento = new CustomEvent("PlayerWon")
+        {
+            { "player_id", playerId },
+            { "score", finalScore }
+        };
+
+        AnalyticsService.Instance.RecordEvent(evento);
+        AnalyticsService.Instance.Flush(); // Opcional
+        Debug.Log($"Evento PlayerWon enviado: player_id={playerId}, score={finalScore}");
+    }
 }
+
